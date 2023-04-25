ce2fa7a70275da42401ce762eb0d4a0b781f6021
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-04-24 22:41:29 -0700

    Restructure file into frontend and backend

--- frontend/src/components/Content/Books.tsx
+++ frontend/src/components/Content/Books.tsx
@@ -1,10 +1,5 @@
-import { useQuery } from "@apollo/client";
-import { GET_BOOKS } from "./queries";
-
 const Books = () => {
-  const { loading, error, data } = useQuery(GET_BOOKS);
-
-  return <div></div>;
+  return <div>poop</div>;
 };
 
 export default Books;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import App from "./App.tsx";
 import "./index.css";
-
-const client = new ApolloClient({
-  uri: "http://localhost:4000",
-  cache: new InMemoryCache(),
-});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
-);
+root.render(<App />);
