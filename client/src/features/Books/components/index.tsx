0e73a8f07c18094930eccda76ed6de934f6d6ae5
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-09-23 19:07:17 -0700

    Rename component

--- client/src/features/Books/components/index.tsx
+++ client/src/features/Books/components/index.tsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { useGetUserBooksQuery } from "../../../services/books";
 import { Book } from "../../../types";
-import Card from "./Book";
+import Card from "./Card";
 import { RootState } from "../../../store";
 import auth from "../../../auth/config";
 import UserSettings from "../../UserSettings/components";
@@ -25,7 +25,7 @@ const Books = () => {
 
   return (
     <main
-      className={`flex flex-col items-center transition-all duration-300 lg:grid  lg:grid-cols-3 lg:p-8 2xl:grid-cols-4 ${
+      className={`flex flex-col items-center transition-all duration-300 lg:grid lg:grid-cols-3 lg:p-8 2xl:grid-cols-4 ${
         showSort ? "mt-72" : "mt-16"
       }`}
     >
