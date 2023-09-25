201f8e1fe2fc46452df78205f53a7ae6ec209024
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-09-25 13:59:05 -0700

    Add router setup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,25 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { store } from "./store.js";
 import { Provider } from "react-redux";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
 );
