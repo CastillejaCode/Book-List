a8ff5c54b13d9f3643d9f8620fa0e14f48f65f2a
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-10-06 23:54:47 -0700

    Add conditional classes

--- client/src/components/Toast.tsx
+++ client/src/components/Toast.tsx
@@ -2,24 +2,36 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { resetToast } from "../slices/notificationSlice";
 import { RootState } from "../store";
+import clsx from "clsx";
 
 const Toast = () => {
   const dispatch = useDispatch();
-  const toast = useSelector((state: RootState) => state.notification);
+  const { message, type } = useSelector(
+    (state: RootState) => state.notification
+  );
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       dispatch(resetToast());
     }, 5000);
     return () => clearTimeout(timeout);
-  }, [dispatch, toast]);
+  }, [dispatch, message]);
 
-  if (!toast.message) return <></>;
+  const style = {
+    notification: "bg-zinc-100 dark:bg-zinc-800",
+    error: "bg-red-700 dark:bg-red-900 text-red-200",
+  };
+
+  if (!message) return <></>;
   return (
     <div
-      className={`toast fixed bottom-8 left-1/2 w-fit -translate-x-1/2 rounded-lg bg-zinc-100 px-6 py-4 shadow-xl transition-all duration-300 dark:bg-zinc-800`}
+      className={clsx(
+        "toast fixed bottom-8 left-1/2 flex w-fit -translate-x-1/2 flex-row gap-6 rounded-lg px-6 py-4 font-semibold shadow-xl ",
+        style[type]
+      )}
     >
-      {toast.message}
+      <p>{message}</p>
+      <button onClick={() => dispatch(resetToast())}>X</button>
     </div>
   );
 };
