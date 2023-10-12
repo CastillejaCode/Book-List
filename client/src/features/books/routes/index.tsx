03b8e5fb7f86cbefc28969d93572804c9f3ee979
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-10-11 23:00:44 -0700

    Refactor to make use of routing

--- client/src/features/books/routes/index.tsx
+++ client/src/features/books/routes/index.tsx
@@ -1,13 +1,13 @@
-import { useSelector } from "react-redux";
-import { useGetUserBooksQuery } from "src/services/books";
-import { Book } from "src/types";
-import Card from "src/features/books/components/Card";
-import { RootState } from "src/store";
-import auth from "src/auth/config";
-import UserSettings from "src/features/user/components";
-import { useAuthState } from "react-firebase-hooks/auth";
 import { skipToken } from "@reduxjs/toolkit/dist/query";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useSelector } from "react-redux";
+import auth from "src/auth/config";
+import Card from "src/features/books/components/Card";
 import useSort from "src/hooks/useSort";
+import { useGetUserBooksQuery } from "src/services/books";
+import { RootState } from "src/store";
+import { Book } from "src/types";
+import Menu from "../components/Menu";
 
 export default function Books() {
   const [user] = useAuthState(auth);
@@ -16,32 +16,23 @@ export default function Books() {
   );
   const books = useSort(data);
 
-  const showUserSettings = useSelector((state: RootState) => state.toggle.user);
   const showSort = useSelector((state: RootState) => state.toggle.sort);
-  const showUser = useSelector((state: RootState) => state.toggle.user);
 
-  // if (isLoading) return <div>Still Loading</div>;
-  // if (isError || !books) return <div>Sorry, something went wrong!</div>;
+  if (isLoading) return <div>Still Loading</div>;
+  if (isError || !books) return <div>Sorry, something went wrong!</div>;
 
   return (
-    <div>stuff</div>
-    // <main
-    //   className={`flex flex-col place-items-center items-center transition-all duration-300 lg:grid lg:grid-cols-3 lg:p-8 2xl:grid-cols-4 ${
-    //     showSort ? "mt-72" : "mt-16"
-    //   }`}
-    // >
-    //   {books.length < 1 && !showUser && (
-    //     <div className="mt-4 text-lg font-light">
-    //       It's empty here, add something!
-    //     </div>
-    //   )}
-    //   {!showUserSettings ? (
-    //     books.map((book: Book) => {
-    //       return <Card book={book} key={book.id} />;
-    //     })
-    //   ) : (
-    //     <UserSettings />
-    //   )}
-    // </main>
+    <>
+      <Menu />
+      <main
+        className={`flex flex-col place-items-center items-center transition-all duration-300 lg:grid lg:grid-cols-3 lg:p-8 2xl:grid-cols-4 ${
+          showSort ? "mt-72" : "mt-16"
+        }`}
+      >
+        {books.map((book: Book) => {
+          return <Card book={book} key={book.id} />;
+        })}
+      </main>
+    </>
   );
 }
