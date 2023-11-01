aadd553b2bd42ec9b08a259c55daf29d1d9bb807
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-11-01 13:24:18 -0700

    Add dialog for new way to access info

--- client/src/features/books/components/Card/Info/index.tsx
+++ client/src/features/books/components/Card/Info/index.tsx
@@ -1,65 +1,13 @@
-import Image from "./Image";
-import Rating from "./Rating";
-
-import {
-  ChevronDownIcon,
-  EllipsisVerticalIcon,
-  CheckCircleIcon,
-} from "@heroicons/react/24/outline";
-
-import { Book } from "../../../../../types";
-
-interface Props extends Book {
-  handleOptions: () => void;
-  handleReview: () => void;
-  handleImage: () => void;
-  showReview: boolean;
-}
-
-export default function Info({
-  title,
-  author,
-  rating,
-  read,
-  coverNumber,
-  handleOptions,
-  handleReview,
-  handleImage,
-  showReview,
-}: Props) {
+export default function Info() {
   return (
     <>
-      <Image
-        title={title}
-        author={author}
-        coverNumber={coverNumber}
-        handleImage={handleImage}
-      />
-      <div className={`flex flex-auto flex-col transition-all duration-300`}>
-        <h2 className="self-start text-2xl font-semibold">{title}</h2>
-        <h3 className=" text-xl">{author}</h3>
-        <div
-          className="flex flex-grow cursor-pointer flex-col justify-end"
-          onClick={handleReview}
-        >
-          <div className="flex justify-center ">
-            <ChevronDownIcon
-              className={` aspect-square w-5 self-end transition-transform duration-300 ${
-                showReview && "rotate-180"
-              }`}
-            />
-          </div>
-        </div>
+      <div className="modal-box">
+        <h3 className="text-lg font-bold">Hello!</h3>
+        <p className="py-4">Press ESC key or click outside to close</p>
       </div>
-      <div className="flex flex-col justify-between ">
-        <button className="aspect-square w-6 self-end" onClick={handleOptions}>
-          <EllipsisVerticalIcon />
-        </button>
-        <div className="flex flex-col items-end gap-2">
-          {read && <CheckCircleIcon className=" aspect-square w-5" />}
-          <Rating rating={rating} />
-        </div>
-      </div>
+      <form method="dialog" className="modal-backdrop">
+        <button>close</button>
+      </form>
     </>
   );
 }
--- client/src/features/books/components/Card/index.tsx
+++ client/src/features/books/components/Card/index.tsx
@@ -5,16 +5,18 @@ import Review from "./UnderCard/Review";
 import Options from "./UnderCard/Options";
 import UnderCard from "./UnderCard";
 import EditForm from "./EditForm";
-import Info from "./Info";
+import Content from "./Content";
 import LeftRight from "./UnderCard/LeftRight";
 import { ImageContext } from "./imageContext";
+import Info from "./Info";
 
 const Card = ({ book }: { book: Book }) => {
   const [showReview, setShowReview] = useState(false);
   const [showOptions, setShowOptions] = useState(false);
   const [showEdit, setShowEdit] = useState(false);
   const [showImageControls, setShowImageControls] = useState(false);
-  const [height, setHeight] = useState(52);
+  const dialogRef = useRef<HTMLDialogElement>(null);
+  // const [height, setHeight] = useState(52);
 
   const { title, author } = book;
   const {
@@ -24,11 +26,11 @@ const Card = ({ book }: { book: Book }) => {
   } = useGetBookImageURLQuery({ title, author });
 
   // To find out height on paint, to allow for responsive sliding of review card
-  const cardRef = useRef<HTMLDivElement>(null);
-  useLayoutEffect(() => {
-    if (!cardRef.current) throw Error("ref is not assigned");
-    setHeight(cardRef.current.clientHeight);
-  }, []);
+  // const cardRef = useRef<HTMLDivElement>(null);
+  // useLayoutEffect(() => {
+  //   if (!cardRef.current) throw Error("ref is not assigned");
+  //   setHeight(cardRef.current.clientHeight);
+  // }, []);
 
   // toggling for each card that can't be dones with redux
   const toggleReview = () => {
@@ -59,29 +61,26 @@ const Card = ({ book }: { book: Book }) => {
   return (
     // Extra div to allow for Review to hide behind main card
 
-    <div
-      ref={cardRef}
-      className={`relative z-0 my-4 w-10/12 max-w-sm shadow-md transition-all duration-300 
-        ${(showReview || showOptions || showImageControls) && ""}`}
+    <article
+      onClick={() => dialogRef.current?.showModal()}
+      className={
+        "relative flex rounded-lg border-2 border-zinc-700 bg-zinc-100 p-3 dark:bg-zinc-800"
+      }
     >
-      <div
-        className={`relative flex rounded-lg border-2 border-zinc-700 bg-zinc-100 p-3 dark:bg-zinc-800 `}
-      >
-        {!showEdit ? (
-          <ImageContext.Provider value={{ docs, isLoading, isError }}>
-            <Info
-              {...book}
-              handleReview={toggleReview}
-              handleOptions={toggleOptions}
-              handleImage={toggleImageControls}
-              showReview={showReview}
-            />
-          </ImageContext.Provider>
-        ) : (
-          <EditForm book={book} toggleEdit={toggleEdit} />
-        )}
-      </div>
-      <UnderCard height={height} showReview={showReview}>
+      <ImageContext.Provider value={{ docs, isLoading, isError }}>
+        <Content
+          {...book}
+          handleReview={toggleReview}
+          handleOptions={toggleOptions}
+          handleImage={toggleImageControls}
+          showReview={showReview}
+        />
+      </ImageContext.Provider>
+      <dialog ref={dialogRef} className="modal">
+        <Info />
+      </dialog>
+
+      {/* <UnderCard height={height} showReview={showReview}>
         <Review review={book.review} />
       </UnderCard>
       <UnderCard height={height} showOptions={showOptions}>
@@ -89,8 +88,8 @@ const Card = ({ book }: { book: Book }) => {
       </UnderCard>
       <UnderCard height={height} showImageControls={showImageControls}>
         <LeftRight id={book.id} coverNumber={book.coverNumber} docs={docs} />
-      </UnderCard>
-    </div>
+      </UnderCard> */}
+    </article>
   );
 };
 
