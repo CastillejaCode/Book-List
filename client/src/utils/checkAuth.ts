d0847cc514b8b1abecf1bf67ec4eaefa1a559d3e
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-11-07 15:34:56 -0800

    Remove failed attempt at using loader for auth

--- client/src/utils/checkAuth.ts
+++
@@ -1,9 +0,0 @@
-import auth from "src/auth/config";
-
-export const checkAuth = async () => {
-  if (!auth.currentUser) {
-    throw new Response("No Authentication", {
-      status: 401,
-    });
-  }
-};
