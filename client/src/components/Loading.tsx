96b6d56110e7782e2d4a17d3a32c0814bbe01e52
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-11-07 15:50:02 -0800

    Create loading page

---
+++ client/src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="grid h-screen place-content-center">
+      <span className="loading-x loading loading-ring loading-lg"></span>
+    </div>
+  );
+}
