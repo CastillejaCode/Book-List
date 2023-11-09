c5286fd6605aebdcb0956baa521d0aba76152626
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-11-08 17:55:29 -0800

    Copy edit form to add form, add more fields

--- client/src/features/books/components/AddForm/index.tsx
+++ client/src/features/books/components/AddForm/index.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useDispatch } from "react-redux";
@@ -15,11 +16,13 @@ interface Props {
 export default function AddForm({ toggleOpen }: Props) {
   const dispatch = useDispatch();
 
-  const [title, setTitle] = useField({ id: "title", type: "text" });
-  const [author, setAuthor] = useField({ id: "author", type: "text" });
-  const [rating, setRating] = useField({ id: "rating", type: "number" });
-  const [review, setReview] = useField({ id: "review", type: "text" });
-  const [read, setRead] = useState(true);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [rating, setRating] = useState(0);
+  const [review, setReview] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [read, setRead] = useState(false);
 
   const [user] = useAuthState(auth);
   const [addBook] = useAddBookMutation();
@@ -29,11 +32,11 @@ export default function AddForm({ toggleOpen }: Props) {
     if (!user) return;
     try {
       addBook({
-        title: title.value,
-        author: author.value,
-        rating: Number(rating.value),
-        read: read,
-        review: review.value,
+        title,
+        author,
+        rating,
+        read,
+        review,
         date: Date.now(),
         uid: user?.uid,
         coverNumber: 0,
@@ -44,7 +47,7 @@ export default function AddForm({ toggleOpen }: Props) {
 
     setTitle("");
     setAuthor("");
-    setRating("0");
+    setRating(0);
     setReview("");
     toggleOpen();
     dispatch(setUndoStatus(false));
@@ -53,63 +56,91 @@ export default function AddForm({ toggleOpen }: Props) {
   return (
     <form
       onSubmit={submitForm}
-      className="flex flex-col items-center gap-4 p-8 "
+      className="flex flex-col gap-4 text-base transition-all duration-300"
     >
-      <h1 className="text-center text-3xl font-medium">Add Book</h1>
-      <label className="flex w-full flex-col">
-        title *
+      <h1 className="self-center text-2xl">Add Book</h1>
+      <label>
+        Title
         <input
-          className="w-full rounded-md border-2 border-zinc-500 px-2 py-1"
-          {...title}
-          required
-          autoFocus
+          className="w-full rounded-md border-2 border-gray-800/70 pl-2 "
+          type="text"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
         />
       </label>
-
-      <label className="flex w-full flex-col">
-        author *
+      <label htmlFor="author">
+        Author
         <input
-          className="w-full rounded-md border-2 border-zinc-500 px-2 py-1"
-          {...author}
-          required
+          className="w-full rounded-md border-2 border-gray-800/70 pl-2"
+          id="author"
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
         />
       </label>
-
-      <div className="flex w-full justify-around ">
-        <label className="flex flex-col items-center gap-1">
-          rating *
-          <input
-            className="w-16 rounded-md border-2 border-zinc-500 pl-2"
-            {...rating}
-            min="1"
-            max="5"
-            step=".5"
-            required
-          />
+      <div>
+        <label className="flex justify-between gap-2" htmlFor="rating">
+          Rating
+          <span>{rating}</span>
         </label>
-        <div className=" flex flex-col items-center justify-between">
-          <label className="flex flex-col items-center  gap-1">
-            read?
-            <input
-              className="checkbox bg-zinc-200"
-              type="checkbox"
-              defaultChecked={read}
-              onChange={(e) => setRead(e.target.checked)}
-            />
-          </label>
+        <input
+          type="range"
+          min="0"
+          max="5"
+          id="rating"
+          value={rating}
+          className="range range-sm"
+          step=".5"
+          onChange={(event) => setRating(Number(event.target.value))}
+        />
+        <div className="flex w-full justify-between px-2 text-xs">
+          <span>|</span>
+          <span>|</span>
+          <span>|</span>
+          <span>|</span>
+          <span>|</span>
+          <span>|</span>
         </div>
       </div>
-      <label className="mb-4 flex h-fit w-full flex-col">
-        review
+      <div className="flex flex-col justify-between gap-4 sm:flex-row">
+        <label className="flex flex-col">
+          Start Date
+          <input
+            type="date"
+            value={startDate}
+            onChange={(event) => setStartDate(event.target.value)}
+            className="w-full rounded-md border-2 border-gray-800/70 pl-2"
+          />
+        </label>
+        <label className="flex flex-col">
+          Finish Date
+          <input
+            type="date"
+            value={endDate}
+            onChange={(event) => setEndDate(event.target.value)}
+            className="w-full rounded-md border-2 border-gray-800/70 pl-2"
+          />
+        </label>
+      </div>
+      <label className="mt-4 flex w-full items-center justify-between gap-4 ">
+        <span className={clsx("flex-1 text-end")}>Not Read</span>
+        <input
+          type="checkbox"
+          checked={read}
+          onChange={(event) => setRead(event.target.checked)}
+          className="toggle"
+        />
+        <span className="flex-1">Read</span>
+      </label>
+      <label>
+        Review
         <textarea
-          className="h-36 rounded-md border-2 border-zinc-500 p-2"
-          spellCheck
-          {...review}
-          cols={20}
-          rows={2}
+          className="mb-2 w-full rounded-md border-2 border-gray-800/70 px-2"
+          id="review"
+          rows={8}
+          value={review}
+          onChange={(event) => setReview(event.target.value)}
         />
       </label>
-      <SubmitButton />
     </form>
   );
 }
