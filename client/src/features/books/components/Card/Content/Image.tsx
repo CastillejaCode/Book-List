48f92311ad4b4debb5e92c54942e0b9947d7e43b
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-11-16 00:02:54 -0800

    Refactor image to better process async info

--- client/src/features/books/components/Card/Content/Image.tsx
+++ client/src/features/books/components/Card/Content/Image.tsx
@@ -1,28 +1,15 @@
-import { BookOpenIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
 import { useImageContext } from "../imageContext";
 
 interface Props {
   title: string;
   author: string;
   coverNumber: number;
-  handleImage: () => void;
 }
 
-const Image = ({ title, coverNumber, handleImage }: Props) => {
+const Image = ({ title, coverNumber }: Props) => {
   const { docs, isLoading, isError } = useImageContext();
 
-  if (isLoading)
-    return (
-      <div className="loading btn-square btn mr-4 aspect-[1/1.5] h-full w-5/12 rounded-md border-2 border-gray-900"></div>
-    );
-  if (true)
-    return (
-      <div className="mr-4 flex aspect-[1/1.5] w-5/12 justify-center rounded-md border-2 border-gray-900">
-        <BookOpenIcon />
-      </div>
-    );
-
-  // Image options
   const value = docs?.at(coverNumber)?.cover_i;
   const key = "id";
   const size = "M";
@@ -30,10 +17,12 @@ const Image = ({ title, coverNumber, handleImage }: Props) => {
 
   return (
     <img
-      src={imgSrc}
+      src={isError ? "book-open.svg" : imgSrc}
       alt={`Book cover for ${title}`}
-      className="mr-4 aspect-[1/1.5] w-5/12 rounded-md border-2 border-gray-700"
-      onClick={handleImage}
+      className={clsx(
+        "aspect-[1/1.5] w-5/12 rounded-md border-2 border-gray-700",
+        isLoading && "loading"
+      )}
     />
   );
 };
