0095735cbf2f52953706d12c119a5e37c1ffec7c
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-11-15 16:51:51 -0800

    Move categorizing to header

--- client/src/features/books/components/NavBar/index.tsx
+++ client/src/features/books/components/NavBar/index.tsx
@@ -4,20 +4,24 @@ import {
   Bars3Icon,
   XMarkIcon,
 } from "@heroicons/react/24/solid";
-import { useRef } from "react";
+import clsx from "clsx";
+import { useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import Filter from "src/features/books/components/Filter";
 import Menu from "src/features/books/components/Menu";
 import { useAddBookMutation } from "src/services/books";
 import { resetSearch, setSearch } from "src/slices/searchSlice";
-import { toggleMenu, toggleSearch, toggleSort } from "src/slices/toggleSlice";
+import { toggleMenu, toggleSearch } from "src/slices/toggleSlice";
 import { toggleUndoStatus } from "src/slices/undoSlice";
 import { RootState } from "src/store";
 
-const NavBar = () => {
+interface Props {
+  children: React.ReactNode;
+}
+
+const NavBar = ({ children }: Props) => {
   const dispatch = useDispatch();
-  const showSort = useSelector((state: RootState) => state.toggle.sort);
+  const [showCategorize, setShowCategorize] = useState(false);
   const showMenu = useSelector((state: RootState) => state.toggle.menu);
   const showUndo = useSelector((state: RootState) => state.undo.status);
   const showSearch = useSelector((state: RootState) => state.toggle.search);
@@ -39,58 +43,68 @@ const NavBar = () => {
 
   return (
     <div className="fixed top-0 z-20">
-      <header className="sticky flex w-screen items-center justify-between border-b border-gray-900 bg-zinc-200 px-4 pb-2 pt-2 shadow-md dark:border-zinc-200 dark:bg-zinc-900">
-        <button
-          onClick={() => {
-            dispatch(resetSearch());
-            if (showSearch) dispatch(toggleSearch());
-            else dispatch(toggleMenu());
-          }}
-          className="relative"
-        >
-          <Bars3Icon
-            className={` aspect-square w-10 transition-all duration-300 
-            ${showMenu || showSearch ? "invisible rotate-90 opacity-0" : ""}`}
-          />
-          <XMarkIcon
-            className={`absolute left-0 top-0 aspect-square w-10 transition-all duration-300 
-            ${!showMenu && !showSearch ? "invisible -rotate-90 opacity-0" : ""}
-            `}
-          />
-        </button>
-        <div className="relative">
-          <Link
-            to="/home"
-            className={`self-baseline text-3xl font-semibold tracking-wide transition-all duration-300
-            ${showSearch && "invisible opacity-0"}
-            `}
+      <header className="flex w-screen flex-col gap-2 border-b border-gray-900 bg-zinc-200 px-4 pb-2 pt-2 shadow-md dark:border-zinc-200 dark:bg-zinc-900">
+        <div className="flex justify-between">
+          <button
+            onClick={() => {
+              dispatch(resetSearch());
+              if (showSearch) dispatch(toggleSearch());
+              else dispatch(toggleMenu());
+            }}
+            className="relative"
           >
-            tomeTracker
-          </Link>
-          <input
-            onChange={(event) => dispatch(setSearch(event.target.value))}
-            value={searchTerm}
-            type="text"
-            ref={inputRef}
-            className={`w- input-bordered input absolute left-1/2 top-0 h-full w-52 -translate-x-1/2 text-lg transition-all duration-300
-            ${!showSearch && "invisible opacity-0"}
-            `}
-          />
+            <Bars3Icon
+              className={` aspect-square w-10 transition-all duration-300
+              ${showMenu || showSearch ? "invisible rotate-90 opacity-0" : ""}`}
+            />
+            <XMarkIcon
+              className={`absolute left-0 top-0 aspect-square w-10 transition-all duration-300
+              ${
+                !showMenu && !showSearch ? "invisible -rotate-90 opacity-0" : ""
+              }
+              `}
+            />
+          </button>
+          <div className="relative">
+            <Link
+              to="/home"
+              className={`self-baseline text-3xl font-semibold tracking-wide transition-all duration-300
+              ${showSearch && "invisible opacity-0"}
+              `}
+            >
+              tomeTracker
+            </Link>
+            <input
+              onChange={(event) => dispatch(setSearch(event.target.value))}
+              value={searchTerm}
+              type="text"
+              ref={inputRef}
+              className={`w- input-bordered input absolute left-1/2 top-0 h-full w-52 -translate-x-1/2 text-lg transition-all duration-300
+              ${!showSearch && "invisible opacity-0"}
+              `}
+            />
+          </div>
+          <button
+            onClick={() => {
+              setShowCategorize(!showCategorize);
+            }}
+          >
+            <AdjustmentsHorizontalIcon
+              className={clsx(
+                "aspect-square w-8 transition-all duration-300",
+                showCategorize && "-rotate-90"
+              )}
+            />
+          </button>
+          <button
+            onClick={addUndoBook}
+            className={`absolute right-16 aspect-square w-8 transition-all duration-300
+              ${showUndo ? "visible opacity-100" : "invisible opacity-0"}`}
+          >
+            <ArrowUturnDownIcon />
+          </button>
         </div>
-        <button onClick={() => dispatch(toggleSort())}>
-          <AdjustmentsHorizontalIcon
-            className={`aspect-square w-8 transition-all duration-300 ${
-              showSort && "-rotate-90"
-            }`}
-          />
-        </button>
-        <button
-          onClick={addUndoBook}
-          className={`absolute right-16 aspect-square w-8 transition-all duration-300 
-            ${showUndo ? "visible opacity-100" : "invisible opacity-0"}`}
-        >
-          <ArrowUturnDownIcon />
-        </button>
+        <div className={clsx(!showCategorize && "hidden")}>{children}</div>
       </header>
       <Menu showMenu={showMenu} focusInput={focusInput} />
     </div>
--- client/src/features/books/routes/index.tsx
+++ client/src/features/books/routes/index.tsx
@@ -1,19 +1,16 @@
 import { skipToken } from "@reduxjs/toolkit/dist/query";
+import { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useSelector } from "react-redux";
 import auth from "src/auth/config";
+import ErrorPage from "src/components/Error/ErrorPage";
 import Loading from "src/components/Loading";
+import Toast from "src/components/Toast";
 import Card from "src/features/books/components/Card";
 import useSort from "src/hooks/useSort";
 import { useGetUserBooksQuery } from "src/services/books";
-import { RootState } from "src/store";
-import { Book, Sort, Filter, Order } from "src/types";
-import NavBar from "../components/NavBar";
-import ErrorPage from "src/components/Error/ErrorPage";
-import Toast from "src/components/Toast";
+import { Book, Filter, Order, Sort } from "src/types";
 import Categorize from "../components/Categorize";
-import { useState } from "react";
-import { setFilter } from "src/slices/categorizeSlice";
+import NavBar from "../components/NavBar";
 
 export default function Books() {
   const [user] = useAuthState(auth);
@@ -25,26 +22,22 @@ export default function Books() {
   const { data, isLoading, isError } = useGetUserBooksQuery(
     user?.uid ?? skipToken
   );
-  const books = useSort({ data, sort, filter, order });
 
-  const showSort = useSelector((state: RootState) => state.toggle.sort);
+  const books = useSort({ data, sort, filter, order });
 
   if (isLoading) return <Loading />;
   if (isError) return <ErrorPage suppliedError="Can't find your books" />;
 
   return (
     <>
-      <NavBar />
-      <main
-        className={`flex min-h-screen flex-col place-items-center items-center gap-4 p-4 transition-all duration-300 lg:grid lg:grid-cols-3 lg:p-8 2xl:grid-cols-4 ${
-          showSort ? "mt-72" : "mt-16"
-        }`}
-      >
+      <NavBar>
         <Categorize
           state={{ sort, filter, order }}
           setState={{ setSort, setFilter, setOrder }}
         />
-        <div>
+      </NavBar>
+      <main className="mt-14 flex min-h-screen flex-col place-items-center items-center gap-4 p-4 transition-all duration-300 ">
+        <div className="grid grid-flow-row gap-4">
           {books &&
             books.map((book: Book) => {
               return <Card book={book} key={book.id} />;
