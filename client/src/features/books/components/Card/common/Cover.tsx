18e7c03245ee4f9962cdc70a5d232682cac3dd54
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-11-16 11:37:34 -0800

    Move and update component

--- client/src/features/books/components/Card/common/Cover.tsx
+++ client/src/features/books/components/Card/common/Cover.tsx
@@ -3,16 +3,15 @@ import { useImageContext } from "../imageContext";
 
 interface Props {
   title: string;
-  author: string;
   coverNumber: number;
+  size: "S" | "M" | "L";
 }
 
-const Image = ({ title, coverNumber }: Props) => {
+export default function Cover({ title, coverNumber, size }: Props) {
   const { docs, isLoading, isError } = useImageContext();
 
   const value = docs?.at(coverNumber)?.cover_i;
   const key = "id";
-  const size = "M";
   const imgSrc = `https://covers.openlibrary.org/b/${key}/${value}-${size}.jpg`;
 
   return (
@@ -20,11 +19,9 @@ const Image = ({ title, coverNumber }: Props) => {
       src={isError ? "book-open.svg" : imgSrc}
       alt={`Book cover for ${title}`}
       className={clsx(
-        "aspect-[1/1.5] w-5/12 rounded-md border-2 border-gray-700",
+        "aspect-[1/1.5] rounded-md border-2 border-gray-700",
         isLoading && "loading"
       )}
     />
   );
-};
-
-export default Image;
+}
