ec3491a40501f609ef1d664415a0fd6c645d3e8d
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-11-20 13:55:34 -0800

    Add conditional for image size

--- client/src/features/books/components/Card/common/Cover.tsx
+++ client/src/features/books/components/Card/common/Cover.tsx
@@ -19,8 +19,10 @@ export default function Cover({ title, coverNumber, size }: Props) {
       src={value ? imgSrc : "book-open.svg"}
       alt={`Book cover for ${title}`}
       className={clsx(
-        "aspect-[1/1.5] w-[8rem] rounded-md border-2 border-gray-700",
-        isLoading && "loading"
+        "aspect-[1/1.5] rounded-md border-2 border-gray-700",
+        isLoading && "loading",
+        // I'm throwing this here so I don't have to add more props to fix the sizing issue
+        size === "M" && "w-[8rem]"
       )}
     />
   );
