91f5e40f7af3784d75370afb93a59203e3971bf6
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-11-21 20:13:47 -0800

    Replace delete dialog with common component

--- client/src/features/user/components/UserSettings/Delete/index.tsx
+++ client/src/features/user/components/UserSettings/Delete/index.tsx
@@ -1,10 +1,63 @@
-import ConfirmDialog from "./ConfirmDialog";
+import { deleteUser } from "firebase/auth";
+import { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import auth from "src/auth/config";
+import ConfirmChoice from "src/components/ui/ConfirmChoice";
+import { useDeleteAllBooksMutation } from "src/services/books";
+import { setToast } from "src/slices/toastSlice";
 
 const Delete = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [user] = useAuthState(auth);
+  const [deleteAllBooks] = useDeleteAllBooksMutation();
+
+  const [visible, setVisible] = useState(false);
+  const [account, setAccount] = useState(false);
+
+  const deleteData = async () => {
+    if (!user) return;
+    const uid = user.uid;
+    await deleteAllBooks({ uid });
+    dispatch(setToast({ message: "Data deleted", type: "error" }));
+  };
+
+  const deleteAccount = async () => {
+    try {
+      if (!user) return;
+      await deleteData();
+      await deleteUser(user);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleData = () => {
+    setVisible(true);
+    setAccount(false);
+  };
+
+  const handleAccount = () => {
+    setVisible(true);
+    setAccount(true);
+  };
+
   return (
     <div className="flex flex-col items-center gap-10 py-4">
-      <ConfirmDialog data />
-      <ConfirmDialog account />
+      <ConfirmChoice
+        visible={visible}
+        setVisible={setVisible}
+        confirmAction={account ? deleteAccount : deleteData}
+      />
+      <button className="btn-error btn" onClick={handleData}>
+        Delete data
+      </button>
+      <button className="btn-error btn" onClick={handleAccount}>
+        Delete account
+      </button>
     </div>
   );
 };
