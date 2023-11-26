f79774848dad827a566edfc1795ca81f39429a18
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-11-25 18:07:18 -0800

    Create new function for checking verification

--- client/src/features/user/components/UserSettings/Account/Verify.tsx
+++ client/src/features/user/components/UserSettings/Account/Verify.tsx
@@ -1,32 +1,31 @@
-import { useDispatch } from "react-redux";
-import { useAuthState } from "react-firebase-hooks/auth";
 import { sendEmailVerification } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useDispatch } from "react-redux";
 import auth from "src/auth/config";
 import { setToast } from "src/slices/toastSlice";
-import useCheckVerify from "src/hooks/useCheckVerify";
+import checkVerification from "src/utils/checkVerification";
 
 export default function Verify() {
   const dispatch = useDispatch();
   const [user] = useAuthState(auth);
-  const checkVerify = useCheckVerify();
 
   const verifyEmail = async (event: React.SyntheticEvent) => {
-    if (!user) return;
     event.preventDefault();
+    if (!user) return;
     try {
       if (user.emailVerified) throw Error("email already verified");
       await sendEmailVerification(user);
-      dispatch(setToast("email sent"));
-      checkVerify.start();
+      checkVerification(2, 60);
+      dispatch(setToast({ message: "email sent" }));
     } catch (error) {
       if (error instanceof Error) dispatch(setToast(error.message));
     }
   };
 
   return (
-    <form className=" flex w-full flex-col" onSubmit={verifyEmail}>
-      <div className="divider"></div>
-      <div className="mb-4 w-fit">
+    <form className=" flex w-full flex-col gap-4" onSubmit={verifyEmail}>
+      <div className="divider my-0"></div>
+      <div className=" w-fit">
         <h2>Verify email account.</h2>
         <h2>Allow adding books after 24 hours.</h2>
       </div>
