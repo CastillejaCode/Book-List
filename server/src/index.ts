01b71b1f9d458732646926d9e93a02c4cd500175
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-04-24 22:54:07 -0700

    Add default port

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import app from './app.js';
 import logger from './utils/logger.js';
 import config from './utils/config.js';
 
-app.listen(config.PORT, () => {
+const PORT = Number(config.PORT) || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
 	logger.info(`Connected to port ${config.PORT}`);
 });
